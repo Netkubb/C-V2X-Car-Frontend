1d92f6775465f62bb695597ff3e2f9575f3678c8
Netkubb <net.chaosanguan@gmail.com>
2025-02-08 20:39:18 +0700

    Integrate preview

---
+++ src/components/View.tsx
@@ -0,0 +1,51 @@
+import React, { RefObject } from "react";
+import Video from "./Video";
+import styles from "./Video.module.css";
+import { WebRTCUser } from "../utils/webRTCUser";
+
+interface ThumbnailViewProps {
+  thumbnailUsers: WebRTCUser[];
+  localThumbnailVideoRef: RefObject<HTMLVideoElement | null>;
+  onVideoClick: (userId: string) => void;
+}
+
+interface DedicatedViewProps {
+  selectedUser: WebRTCUser;
+  onBack: () => void;
+}
+
+export const ThumbnailVideoView: React.FC<ThumbnailViewProps> = ({
+  thumbnailUsers,
+  localThumbnailVideoRef,
+  onVideoClick,
+}) => (
+  <div>
+    <video
+      className={styles.video}
+      muted
+      ref={localThumbnailVideoRef}
+      autoPlay
+    />
+    {thumbnailUsers.map((user) => (
+      <Video
+        key={user.id}
+        stream={user.stream}
+        onVideoClick={onVideoClick}
+        userId={user.id}
+      />
+    ))}
+  </div>
+);
+
+export const DedicatedVideoView: React.FC<DedicatedViewProps> = ({
+  selectedUser,
+  onBack,
+}) => (
+  <div>
+    <Video
+      stream={selectedUser.stream}
+      onVideoClick={onBack}
+      userId={selectedUser.id}
+    />
+  </div>
+);
